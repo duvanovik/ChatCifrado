331723bd33636db173d6ab3fe934c82ac5dd0f8e
duvanovik <duvanovik@outlook.com>
2023-11-28 00:33:04 -0500

    Configuro client y servidor

--- ChatCifrado/src/Client.java
+++ ChatCifrado/src/Client.java
@@ -1,4 +1,77 @@
+import java.net.Socket;
+import java.util.Arrays;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Client {
+    public static void main(String[] args) {
+        String host = "localhost"; // Cambia a la dirección IP del servidor
+        int port = 12345;
 
+        try (Socket socket = new Socket(host, port);
+             DataInputStream dis = new DataInputStream(socket.getInputStream());
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+             BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Conectado al servidor");
+
+            // Iniciar Diffie-Hellman
+            DiffieHellman dh = new DiffieHellman();
+
+            // Recibir clave pública del servidor
+            int keyLength = dis.readInt();
+            byte[] serverPubKey = new byte[keyLength];
+            dis.readFully(serverPubKey);
+            
+            //TEST
+            System.out.println("Clave pública del servidor (recibida): " + Arrays.toString(serverPubKey));
+
+
+            // Enviar clave pública al servidor
+            byte[] publicKey = dh.getEncodedPublicKey();
+            dos.writeInt(publicKey.length);
+            dos.write(publicKey);
+            
+            //TEST
+            System.out.println("Clave pública del cliente (enviada): " + Arrays.toString(publicKey));
+
+            
+            // Generar clave compartida
+            byte[] sharedSecret = dh.generateSharedSecret(serverPubKey);
+            AES aes = new AES(sharedSecret);
+            System.out.println("Clave compartida en el cliente: " + Arrays.toString(sharedSecret));
+
+            // Lógica del chat cifrado
+            boolean continuar = true;
+            while (continuar) {
+                System.out.print("Escribe un mensaje (o 'salir' para finalizar): ");
+                String messageToSend = consoleInput.readLine();
+
+                if ("salir".equalsIgnoreCase(messageToSend)) {
+                    continuar = false;
+                } else {
+                    // Enviar mensaje cifrado
+                    byte[] encryptedMessage = aes.encrypt(messageToSend);
+                    System.out.println("Mensaje cifrado del cliente: " + Arrays.toString(encryptedMessage));
+                    dos.writeInt(encryptedMessage.length);
+                    dos.write(encryptedMessage);
+
+                    // Recibir y descifrar respuesta
+                    int responseLength = dis.readInt();
+                    byte[] encryptedResponse = new byte[responseLength];
+                    dis.readFully(encryptedResponse);
+                    String response = aes.decrypt(encryptedResponse);
+                    System.out.println("Respuesta descifrada en el cliente: " + response);
+                    System.out.println("Servidor responde: " + response);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error en el cliente: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
--- ChatCifrado/src/Server.java
+++ ChatCifrado/src/Server.java
@@ -1,4 +1,88 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class Server {
+    public static void main(String[] args) {
+        int port = 12345;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Servidor esperando conexiones...");
 
+            try (Socket clientSocket = serverSocket.accept();
+                 DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                 DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream())) {
+
+                System.out.println("Cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
+
+                // Iniciar Diffie-Hellman
+                DiffieHellman dh = new DiffieHellman();
+                byte[] publicKey = dh.getEncodedPublicKey();
+
+                // Enviar clave pública al cliente
+                dos.writeInt(publicKey.length);
+                dos.write(publicKey);
+                
+                //TEST
+                System.out.println("Clave publica del servidor (enviada): " + Arrays.toString(publicKey));
+
+                // Recibir clave pública del cliente
+                int keyLength = dis.readInt();
+                byte[] clientPubKey = new byte[keyLength];
+                dis.readFully(clientPubKey);
+                
+                //TEST
+                System.out.println("Clave pública del cliente (recibida): " + Arrays.toString(clientPubKey));
+
+
+                // Generar clave compartida
+                byte[] sharedSecret = dh.generateSharedSecret(clientPubKey);
+                AES aes = new AES(sharedSecret);
+                
+                //TEST
+                System.out.println("Clave compartida en el servidor: " + Arrays.toString(sharedSecret));
+
+                
+                // Lógica del chat cifrado
+                boolean continuar = true;
+                while (continuar) {
+                    try {
+                        // Recibir mensaje cifrado
+                        int messageLength = dis.readInt();
+                        if (messageLength > 0) {
+                            byte[] encryptedMessage = new byte[messageLength];
+                            dis.readFully(encryptedMessage);
+                            String message = aes.decrypt(encryptedMessage);
+                            System.out.println("Mensaje descifrado en el servidor: " + message);
+
+
+                            // Procesar mensaje
+                            System.out.println("Cliente dice: " + message);
+
+                            // Responder al cliente
+                            String response = "Mensaje recibido: " + message;
+                            byte[] encryptedResponse = aes.encrypt(response);
+                            System.out.println("Respuesta cifrada del servidor: " + Arrays.toString(encryptedResponse));
+                            dos.writeInt(encryptedResponse.length);
+                            dos.write(encryptedResponse);
+                        } else {
+                            continuar = false;
+                        }
+                    } catch (IOException e) {
+                        System.out.println("Error al recibir mensaje: " + e.getMessage());
+                        continuar = false;
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error en la conexión: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.out.println("No se pudo iniciar el servidor: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
